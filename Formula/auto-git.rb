c94659a8cfd3dbc3073c1dabb11942200dd40a36
CODY-RAY <crank.20@osu.edu>
2018-05-14 23:31:21 -0700

    Create auto-git.rb
    added Formula for a git auto puller

---
+++ Formula/auto-git.rb
@@ -0,0 +1,10 @@
+class AutoGit < Formula
+  desc "git pulls every 15 mins"
+  homepage ""
+  url "https://github.com/CODY-RAY/auto-git/archive/v0.0.1.tar.gz"
+  sha256 "751584d2f24aed6f5e7652361c419ac451e5162a7e85c4af4fdfe5244c511fe0"
+  def install
+ 	bin.install "auto-git"
+  end
+
+end
